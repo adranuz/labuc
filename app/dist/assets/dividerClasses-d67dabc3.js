d2a1c3d637afce6f0a5a69435ee6b7bfbb3fc587
German Chavarín <gerchavarin@equality.company>
2023-07-04 09:18:23 -0700

    Build

---
+++ app/dist/assets/dividerClasses-d67dabc3.js
@@ -0,0 +1,1 @@
+import{g as t}from"./styled-d13899d2.js";import{g as e}from"./Typography-72a89b57.js";function a(i){return t("MuiDivider",i)}const r=e("MuiDivider",["root","absolute","fullWidth","inset","middle","flexItem","light","vertical","withChildren","withChildrenVertical","textAlignRight","textAlignLeft","wrapper","wrapperVertical"]),d=r;export{d,a as g};
