f66adaad5ef0d7ddd290889935def574fc0206a2
German Chavarín <gerchavarin@equality.company>
2023-09-14 13:27:49 -0700

    Build

--- api/dist/prisma/seed.js
+++ api/dist/prisma/seed.js
@@ -5,7 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const bcryptjs_1 = require("bcryptjs");
 const client_1 = require("@prisma/client");
-const customers_1 = __importDefault(require("./data/customers"));
+// import customers from './data/customers'
+const customers_json_1 = __importDefault(require("./data/customers.json"));
 const prisma = new client_1.PrismaClient();
 const permisionsData = [
     { action: 'user/list' },
@@ -164,7 +165,6 @@ const productsData = [
     },
 ];
 async function main() {
-    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p;
     console.log(`[!] Start seeding ...`);
     console.log(`[+] Creating permissions`);
     for (const p of permisionsData) {
@@ -199,83 +199,39 @@ async function main() {
     }
     console.log(`[+] All products created successfully`);
     console.log(`[+] Creating customers`);
-    for (const customer of customers_1.default) {
-        const products = [];
-        if (customer.credolab === 1) {
-            products.push({ shortName: 'credolab' });
-        }
-        if (customer.lms === 1) {
-            products.push({ shortName: 'lms' });
-        }
-        if (customer.nuovo === 1) {
-            products.push({ shortName: 'nuovo' });
-        }
-        if (customer.protexion === 1) {
-            products.push({ shortName: 'protexion' });
-        }
-        if (customer.pagos === 1) {
-            products.push({ shortName: 'pagos' });
-        }
-        if (customer.entretenimiento === 1) {
-            products.push({ shortName: 'entretenimiento' });
-        }
+    for (const customer of customers_json_1.default) {
+        const { customId, name, email, country, registeredName, rfc, address, economicActivity, status, sellerName, sellerComments, comissionTerm, percentageComissions, devices, skuStart, skuEnd, sku3m, skuHBMF, skuHBMPRE, dbName, contacts, products } = customer;
         const customerCreated = await prisma.customer.create({
             data: {
-                customId: customer.cust_id,
-                name: (_a = customer.nombre) !== null && _a !== void 0 ? _a : '',
-                email: (_b = customer.email) !== null && _b !== void 0 ? _b : '',
-                country: (_c = customer.pais) !== null && _c !== void 0 ? _c : '',
-                registeredName: (_d = customer.razon_social) !== null && _d !== void 0 ? _d : '',
-                rfc: (_e = customer.rfc) !== null && _e !== void 0 ? _e : '',
-                address: (_f = customer.d_fisc) !== null && _f !== void 0 ? _f : '',
-                economicActivity: (_g = customer.giro) !== null && _g !== void 0 ? _g : '',
-                status: (_h = customer.status) !== null && _h !== void 0 ? _h : '',
-                sellerName: (_j = customer.vendedor) !== null && _j !== void 0 ? _j : '',
-                sellerComments: (_k = customer.comentarios) !== null && _k !== void 0 ? _k : '',
-                comissionTerm: (_l = customer.vigencia_comision) !== null && _l !== void 0 ? _l : '',
-                percentageComissions: (_m = customer.porcentaje_comision) !== null && _m !== void 0 ? _m : 0,
+                customId,
+                name,
+                email,
+                country,
+                registeredName,
+                rfc,
+                address,
+                economicActivity,
+                status,
+                sellerName,
+                sellerComments,
+                comissionTerm,
+                percentageComissions,
+                devices,
+                skuStart,
+                skuEnd,
+                sku3m,
+                skuHBMF,
+                skuHBMPRE,
+                dbName,
+                contacts: {
+                    create: contacts
+                },
                 products: {
                     connect: products
-                },
-                dbName: customer.db_name ? customer.db_name : '',
+                }
             }
         });
         console.log(`[-] created customer with id: ${customerCreated.id}`);
-        const contacts = [];
-        if ((customer === null || customer === void 0 ? void 0 : customer.nom_com) && customer.nom_com.length > 0) {
-            contacts.push({
-                name: (_o = customer.nom_com) !== null && _o !== void 0 ? _o : '',
-                email: (_p = customer === null || customer === void 0 ? void 0 : customer.email_com) !== null && _p !== void 0 ? _p : '',
-                type: 'com',
-            });
-        }
-        if ((customer === null || customer === void 0 ? void 0 : customer.email_tec) && customer.email_tec.length > 0) {
-            const emailsTec = customer.email_tec.split(',');
-            emailsTec.forEach((email, i) => {
-                var _a;
-                let nameTec = '';
-                if (customer === null || customer === void 0 ? void 0 : customer.nom_tec) {
-                    const namesTec = customer.nom_tec.split(',');
-                    nameTec = (_a = namesTec[i]) !== null && _a !== void 0 ? _a : customer.nom_tec;
-                }
-                contacts.push({
-                    name: nameTec,
-                    email: email,
-                    type: 'tec',
-                });
-            });
-        }
-        for (const contactData of contacts) {
-            const contact = await prisma.contact.create({
-                data: {
-                    name: contactData.name,
-                    email: contactData.email,
-                    type: contactData.type,
-                    customerId: customerCreated.id,
-                }
-            });
-            console.log(`[-] created contact with id: ${contact.id}`);
-        }
     }
     console.log(`[+] All customers created successfully`);
     console.log(`[!] Seeding finished.`);
